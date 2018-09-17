91908ded9ae6fa1f8b68de28132441a0f83ad978
LuizGuilherme <gui.carvalho1905@gmail.com>
2018-09-16 22:05:57 -0300

    entrada pronta

--- Trabalho1.hs
+++ Trabalho1.hs
@@ -1,1 +1,29 @@
+import Data.Char
+import System.IO
+import Data.List
+import Data.List.Split
+import Data.Char
 
+getFirstTuple (x,_) = x
+getSecondTuple (_,x) = x
+
+
+splitList (x:xs) list
+  | x /= [] = splitList xs (list ++ [x])
+  | x == [] = (list, xs)
+
+
+main :: IO()
+main = do
+    contents <- getContents
+    let
+        input = map words (lines contents)
+        tuple = splitList input []
+        list1 = getFirstTuple tuple
+        aux = getSecondTuple tuple
+        aux2 = splitList aux []
+        list2 = getFirstTuple aux2
+        list3 = getSecondTuple aux2
+    print (list1)
+    print (list2)
+    print (list3)
